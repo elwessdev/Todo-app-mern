f149b577e62627b9b257e614db59947ded1b48fa
Ahmed Haddaji <ahmedhaddajiahmed@gmail.com>
2024-10-21 20:54:20 +0100

    Fix Bugs

--- back/index.js
+++ back/index.js
@@ -1,15 +1,13 @@
 const express = require("express");
-const mongoose = require("mongoose");
+const app = express();
+
 const cors = require("cors");
 const connectDB = require('./config/db');
-const todoModel = require("./Models/todo");
 require ("dotenv").config();
 
-
-const app = express();
 connectDB();
 app.use(cors());
-// app.use(express.json());
+app.use(express.json());
 app.use('/task', require('./Routes/Task'));
 app.use("/",(req,res) => res.send("server is running"));
 
